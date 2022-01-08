02742c896df56036b78252cacffbf52ab42c4ec6
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-01-08 12:23:21 +0300

    imported Service class

--- giphy-clone/src/app/app.component.ts
+++ giphy-clone/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyserviceService } from './myservice.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'giphy-clone';
+ public id = ''
+  constructor(public myserviceService:MyserviceService){
+    this.showData();
+  }
+
+  showData(){
+    this.myserviceService.getData().subscribe((res)=>{
+    
+      console.log(res)
+    
+    })
+  }
 }
