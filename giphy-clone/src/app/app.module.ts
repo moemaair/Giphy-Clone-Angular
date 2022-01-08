866eb96ddc984548982f023c03097f7e4b268c17
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-01-08 12:23:44 +0300

    imported service class

--- giphy-clone/src/app/app.module.ts
+++ giphy-clone/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyserviceService } from './myservice.service';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MyserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
